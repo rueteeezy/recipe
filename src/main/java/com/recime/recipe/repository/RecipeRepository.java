5b4c673e983b1e7f341edb4300260554f197babd
Rueteezy <ruel.romeo.sintinta@gmail.com>
2026-05-07 08:42:36 +0800

    Add repository

---
+++ src/main/java/com/recime/recipe/repository/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.recime.recipe.repository;
+
+
+import com.recime.recipe.entity.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface RecipeRepository extends JpaRepository<Recipe, Long>, JpaSpecificationExecutor<Recipe> {
+}
